a01ba8908f8e7a1c1106166a76f9c0c0fabb60e2
Franco Ortega <francopdx@gmail.com>
2021-05-20 17:09:30 -0700

    set up initial router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 // import logo from './logo.svg';
 import './App.scss';
-// import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import { Router, Route, Switch } from "react-router";
 
 
-function App() {
+const App = () => {
   //Header
   //Home page
   //Welcome/Instructions page
   //Game page
 
   return (
+
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Router>
+        HEADER
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
