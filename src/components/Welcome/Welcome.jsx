0d0d77cbfaf7f50b5ebf6e619d244e5822b03153
Franco Ortega <francopdx@gmail.com>
2021-05-20 23:22:54 -0700

    add prop to Header and Welcome

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Welcome.module.scss';
 
-const Welcome = () => {
+const Welcome = ({ player }) => {
   return (
     <div className={styles.Welcome}>
-      <h2>Welcome!</h2>
+      <h2>Welcome {player}!</h2>
     </div>
   );
 };
+
+Welcome.propTypes = {
+  player: PropTypes.string.isRequired
+};
+
 
 export default Welcome;
