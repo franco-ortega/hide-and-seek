f70cdc20b8c37ff6d22167dc458080f10f299651
Franco Ortega <francopdx@gmail.com>
2021-05-23 09:57:13 -0700

    update Header to display player name after Start Game button is clicked

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
 
-const Header = ({ player }) => {
+const Header = ({ player, gameActive }) => {
   return (
     <div className={styles.Header}>
       <h1>Hide & Seek</h1>
-      <p>{player}</p>
+      <p>{gameActive && player}</p>
     </div>
   );
 };
 
 Header.propTypes = {
-  player: PropTypes.string.isRequired
+  player: PropTypes.string.isRequired,
+  gameActive: PropTypes.string.isRequired
 };
 
 export default Header;
