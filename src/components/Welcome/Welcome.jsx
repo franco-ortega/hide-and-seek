cc6292fa6fb71e6893d6942c7510a84ab9a676f5
Franco Ortega <francopdx@gmail.com>
2021-06-11 18:54:14 -0700

    update rules and associated styling

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -19,13 +19,13 @@ const Welcome = ({ player, gameActive, setGameActive }) => {
     <main className={styles.Welcome}>
       <h2>Welcome {player}!</h2>
       <section>
-        <h3>Instructions</h3>
-        <p>1. Guess the item is hidden by clicking on a box.</p>
-        {/* <p>2. Hide your item by clicking on a box.</p> */}
-        {/* <p>3. The computer will try to find your item.</p> */}
-        <p>4. Every correct guess scores 1 point for you.</p>
-        <p>4. Every incorrect guess scores 1 point for the computer.</p>
-        <p>5. The first one to 5 points wins the game.</p>
+        <h3>Game Play:</h3>
+        <p>1. The computer will hide the item in a box.</p>
+        <p>2. You can guess where the item is hidden by clicking on a box.</p>
+        <p>3. Then you get to hide the item by clicking on a box.</p>
+        <p>4. The computer will try to guess where you hid the item.</p>
+        <p>5. Every correct guess scores 1 point.</p>
+        <p>6. The first one to 3 points wins the game.</p>
         <button onClick={onStartGameClick}>Start Game</button>
       </section>
     </main>
